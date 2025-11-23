b20235bb9c6e250d3f4027e8cf5fed07f835c3bb
Leopoldo Fuentes Cruz <leofuenntes@gmail.com>
2025-11-22 18:30:39 -0600

    JuegoSumas1

--- Assets/Scripts/Scrip Juegos Sumas/Juego1/ICarriable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface ICarriable
-{
-    // Método que cualquier objeto (auto, jugador, CPU) debe implementar
-    // cuando necesite moverse a otro carril
-    void MoveToLane(int laneIndex);
-}
